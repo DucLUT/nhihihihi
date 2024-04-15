e25c728fc92af7233c5e0c17740ec7b987a4a29a
Duc Duong <minhduc16032004@gmail.com>
2024-04-15 20:57:01 +0300

    fix port

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,8 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   base: "/will-you-be-my-valentine",
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: 3000,
+  },
 });
